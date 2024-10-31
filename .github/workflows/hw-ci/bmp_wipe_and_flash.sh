14d1774034b074b8aba37d79d58c2a8ad1434b43
FDSoftware <diego+gh@churrosoft.ar>
2024-10-31 18:10:22 +0000

    updates on hardware-ci job

---
+++ .github/workflows/hw-ci/bmp_wipe_and_flash.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# from: https://unix.stackexchange.com/questions/144029/command-to-determine-ports-of-a-device-like-dev-ttyusb0
+
+vidpid_to_devs(){
+        find $(grep -l "PRODUCT=$(printf "%x/%x" "0x${1%:*}" "0x${1#*:}")" \
+                  /sys/bus/usb/devices/[0-9]*:*/uevent | sed 's,uevent$,,') \
+           /dev/null -name dev -o -name dev_id |
+        sed 's,[^/]*$,uevent,' |
+        xargs sed -n -e s,DEVNAME=,/dev/,p -e s,INTERFACE=,,p
+}
+
+ttyPorts=$(vidpid_to_devs 1d50:6018)
+ttyPortArr=($ttyPorts)
+
+arm-none-eabi-gdb -nx --batch \
+	-ex "target extended-remote ${ttyPortArr}" \
+	-ex 'mon tpwr enable' \
+	-ex 'monitor swdp_scan' \
+	-ex 'attach 1' \
+	-ex 'load' \
+	-ex 'compare-sections' \
+	-ex 'kill' \
+	deliver/rusefi.bin
